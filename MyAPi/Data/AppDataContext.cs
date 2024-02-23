1877d4584964077ee9c75f6c50c3198d0d121836
MiluPandya <100260419+MiluPandya@users.noreply.github.com>
2024-02-23 21:27:25 +0000

    Data Folder Added

---
+++ MyAPi/Data/AppDataContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MyClassLibrary;
+namespace MyAPi.Data;
+
+public class AppDataContext : DbContext
+{
+    public AppDataContext(DbContextOptions<AppDataContext> options)
+        :base(options)
+        {}
+	
+	public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; } 
+
+}
+
